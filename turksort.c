91e9eef1c5411c01dcbafddfd3cf03305c338787
dcheng093 <chengpeter37093@gmail.com>
2026-01-09 00:31:18 +0800

    stuff

---
+++ turksort.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   turksort.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcheng <dcheng@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/14 21:49:04 by dcheng            #+#    #+#             */
+/*   Updated: 2025/12/14 21:49:04 by dcheng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	set_target_a(t_pushswap *ps)
+{
+	size_t	i;
+	size_t	j;
+	int		best_target_val;
+	size_t	target_idx;
+	size_t	max_b_idx;
+
+	max_b_idx = find_max_index(ps->b, ps->size_b);
+	i = 0;
+	while (i < ps->size_a)
+	{
+		best_target_val = INT_MIN;
+		target_idx = max_b_idx;
+		j = 0;
+		while (j < ps->size_b)
+		{
+			if (ps->b[j] < ps->a[i] && ps->b[j] > best_target_val)
+			{
+				best_target_val = ps->b[j];
+				target_idx = j;
+			}
+			j++;
+		}
+		ps->target_a[i] = target_idx;
+		i++;
+	}
+}
+
+void	set_target_b(t_pushswap *ps)
+{
+	size_t	i;
+	int		best_target_val;
+	size_t	target_idx;
+
+	best_target_val = INT_MAX;
+	target_idx = find_min_index(ps->a, ps->size_a);
+	i = 0;
+	while (i < ps->size_a)
+	{
+		if (ps->a[i] > ps->b[0] && ps->a[i] < best_target_val)
+		{
+			best_target_val = ps->a[i];
+			target_idx = i;
+		}
+		i++;
+	}
+	ps->target_b = target_idx;
+}
+
+void	sort_three(t_pushswap *ps)
+{
+	int	max;
+
+	if (ps->size_a > 3)
+		return ;
+	max = ps->a[find_max_index(ps->a, ps->size_a)];
+	if (ps->a[0] == max)
+		op(ps, ROTATE_A);
+	else if (ps->a[1] == max)
+		op(ps, REVERSE_ROTATE_A);
+	if (ps->a[0] > ps->a[1])
+		op(ps, SWAP_A);
+}
+
+void	sort_stack(t_pushswap *ps)
+{
+	int		min_val;
+	size_t	min_idx;
+	size_t	median_a;
+
+	min_idx = find_min_index(ps->a, ps->size_a);
+	min_val = ps->a[min_idx];
+	median_a = (ps->size_a + 1) / 2;
+	if (min_idx < median_a)
+		while (ps->a[0] != min_val)
+			op(ps, ROTATE_A);
+	else
+		while (ps->a[0] != min_val)
+			op(ps, REVERSE_ROTATE_A);
+}
+
+void	turksort(t_pushswap *ps)
+{
+	while (ps->size_a > 3 && !is_sorted(ps->a, ps->size_a))
+	{
+		move_cheapest_to_top(ps);
+		op(ps, PUSH_B);
+	}
+	sort_three(ps);
+	while (ps->size_b > 0)
+	{
+		move_b_target_to_top(ps);
+		op(ps, PUSH_A);
+	}
+	sort_stack(ps);
+}
