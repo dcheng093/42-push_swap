95df8f640f8c6e41111856304e751238e9fccd8b
Daniel Cheng <dcheng@u81z05s06.42kl.edu.my>
2026-01-16 17:18:36 +0800

    stuff

---
+++ Makefile
@@ -0,0 +1,44 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: dcheng <dcheng@student.42kl.edu.my>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/12/11 18:25:40 by dcheng            #+#    #+#              #
+#    Updated: 2025/12/11 18:25:40 by dcheng           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = push_swap
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+LIBFTDIR = libft
+LIBFT = $(LIBFTDIR)/libft.a
+SRC = arg.c arr.c ops_a.c ops_b.c ops.c push_swap.c turksort_cost.c turksort_helpers.c turksort.c 
+OBJS = $(SRC:.c=.o)
+RM = rm -f
+
+all: $(NAME)
+
+$(NAME): $(OBJS) $(LIBFT)
+	$(CC) $(CFLAGS) $(OBJS) -L$(LIBFTDIR) -lft -o $(NAME)
+
+$(LIBFT):
+	$(MAKE) -C $(LIBFTDIR)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -I$(LIBFTDIR) -c $< -o $@
+
+clean:
+	$(RM) $(OBJS)
+	$(MAKE) -C $(LIBFTDIR) clean
+
+fclean: clean
+	$(RM) $(NAME)
+	$(MAKE) -C $(LIBFTDIR) fclean
+
+re: fclean all
+
+.PHONY: all clean fclean re
+
---
+++ libft/ft_printf.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcheng <dcheng@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/14 16:56:04 by dcheng            #+#    #+#             */
+/*   Updated: 2026/01/16 16:57:51 by dcheng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_format(char c, va_list args)
+{
+	if (c == 'c')
+		return (ft_putchar_fd(va_arg(args, int), 1), 1);
+	else if (c == 's')
+		return (ft_putstr_pf(va_arg(args, char *)));
+	else if (c == 'd' || c == 'i')
+		return (ft_putnbr_pf((long)va_arg(args, int)));
+	else if (c == 'u')
+		return (ft_putunbr_pf((unsigned long)va_arg(args, unsigned int)));
+	else if (c == 'p')
+		return (ft_putptr_pf(va_arg(args, void *)));
+	else if (c == 'x')
+		return (ft_puthex_pf(va_arg(args, unsigned int), HEX_LOW));
+	else if (c == 'X')
+		return (ft_puthex_pf(va_arg(args, unsigned int), HEX_UP));
+	else if (c == '%')
+		return (write(1, "%", 1));
+	return (0);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		count;
+
+	count = 0;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			count += ft_format(*format, args);
+		}
+		else
+			count += write(1, format, 1);
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
